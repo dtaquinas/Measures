d032bf1903fe65bee1ee2814dc51ab4186c0ab2d
Dylan Murphy <dtaquinas@gmail.com>
2016-04-06 10:44:37 -0700

    Polynomial evaluation

--- src/poly.cc
+++ src/poly.cc
@@ -1,13 +1,18 @@
-
-
 template<typename coeff_t>
 class Polynomial {
   private:
   std::vector<coeff_t> coefficients;
   public:
-  coeff_t eval( coeff_t z )
+  coeff_t operator()( coeff_t z )
   {
-    
+    coeff_t result = this.coeff.last();
+    std::vector<coeff_t>::reverse_iterator rit;
+    rit++;
+    for (; rit != this.coefficients.end(); ++rit)
+      {
+        result = result * z + *rit;
+      }
+    return result;
   }
   coeff_t roots();
 };
