e5fb7d76864ca0d94f8063eb3ea416d39901c817
Dylan Murphy <dtaquinas@gmail.com>
2016-05-23 13:11:11 -0700

    Polynomials

--- src/poly.cc
+++ src/poly.cc
@@ -1,142 +1,179 @@
-#include <eigen3/Eigen/Core>
+#include <iostream>
+#include <complex>
+#include <vector>
 
-template<class coeff_t>
-class Polynomial<coeff_t> {
+
+// Important note: coefficients go in order of increasing degree: the first element of coeff is the constant, etc.
+template<typename coeff_t>
+class Polynomial {
   private:
   std::vector<coeff_t> coeff;
   public:
-  Polynomial<coeff_t>();
-  coeff_t operator()( coeff_t z )
+  Polynomial();
+  coeff_t operator()( coeff_t z ) 
   {
-    coeff_t result = coeff.last();
-    std::vector<coeff_t>::reverse_iterator rit = coeff.rbegin();
+    coeff_t result = coeff.back();
+    typename std::vector<coeff_t>::reverse_iterator rit = coeff.rbegin();
     rit++;
-    for (; rit != this.coeff.end(); ++rit)
+    for (; rit != coeff.rend(); ++rit)
       {
         result = result * z + *rit;
       }
     return result;
   }
-  coeff_t roots();
-  long set_coeffs( std::vector<coeff_t> rhs )
+  //coeff_t roots();
+  int set_coeffs( std::vector<coeff_t> rhs )
   {
-    std::vector<coeff_t>::reverse_iterator rit = rhs.rbegin();   //Reverse iterate to trim any zeroes, i.e. leading coefficent must be nonzero.
+    typename std::vector<coeff_t>::reverse_iterator rit = rhs.rbegin();   //Reverse iterate to trim any zeroes, i.e. leading coefficent must be nonzero.
     while (*rit == 0 && (rit + 1) != rhs.rend())
     {
-      rhs.pop_back()
+      rhs.pop_back();
       rit++;
     }
     coeff = rhs;
+    return 0;
   }
-  long degree() {return coeff.size()}
-  Polynomial<coeff_t>& operator=(const Polynomial<coeff_t> &rhs);
-  Polynomial<coeff_t> operator+(const Polynomial<coeff_t> &other);
-  Polynomial<coeff_t> operator*(const Polynomial<coeff_t> &other);
-  Polynomial<coeff_t> operator-(const Polynomial<coeff_t> &other);
-  Polynomial<coeff_t> operator+=(const Polynomial<coeff_t> &other);
-  Polynomial<coeff_t> operator*=(const Polynomial<coeff_t> &other);
+  int degree() const {return coeff.size() - 1;}
+
+  // OVERLOAD ALL THE OPERATORS
+
+  Polynomial<coeff_t>& operator=(const Polynomial<coeff_t> &rhs)
+  {
+    if(this == &rhs)
+      return *this;
+    //int newsize;
+    //newsize = rhs.degree() + 1;
+    //coeff.resize(newsize);
+    set_coeffs(rhs.coeff);
+  }
+
+  Polynomial<coeff_t> & operator+=( const Polynomial<coeff_t> &rhs )
+  {
+    if (rhs.degree() > degree())
+    {
+      coeff.resize(rhs.degree());
+    }
+    for (int i = 0; i <= rhs.degree(); i++)
+    {
+      coeff[i] += rhs.coeff[i];
+    }
+    return *this;
+  }
+
+  Polynomial<coeff_t> & operator-=( const Polynomial<coeff_t> &rhs )
+  {
+  if (rhs.degree() > degree())
+    {
+      coeff.resize(rhs.degree());
+    }
+    for (int i = 0; i <= rhs.degree(); i++)
+    {
+      coeff[i] -= rhs.coeff[i];
+    }
+    return *this;
+  }
+
+  Polynomial<coeff_t> & operator*=( const Polynomial<coeff_t> &rhs )
+  {
+    int new_degree = degree() + rhs.degree();
+    std::vector<coeff_t> tempc(new_degree + 1);
+    int i;
+    int j;
+    for (i = 0; i <= new_degree + 1; i++)
+    {
+      for (j = 0; j <= i; j++)
+      {
+        if (j <= degree() && (i - j) <= rhs.degree())
+          tempc[i] += coeff[j] * rhs.coeff[i - j];
+      }
+    }
+    set_coeffs(tempc);
+    return *this;
+  }
+
+
+  Polynomial<coeff_t> & operator*=( const coeff_t &rhs )
+  {
+  std::vector<coeff_t> result = coeff;
+    typename std::vector<coeff_t>::reverse_iterator rit = coeff.rbegin();
+    rit++;
+    for (; rit != coeff.rend(); ++rit)
+      {
+        result = result * rhs;
+      }
+    set_coeffs(result);  
+  }
+
+  Polynomial<coeff_t> operator+(const Polynomial<coeff_t> &rhs)
+  {
+    Polynomial<coeff_t> result = *this;
+    result += rhs;
+    return result;
+  }
+
+  Polynomial<coeff_t> operator-(const Polynomial<coeff_t> &rhs)
+  {
+    Polynomial<coeff_t> result = *this;
+    result -= rhs;
+    return result;
+  }
+
+  Polynomial<coeff_t> operator*(const Polynomial<coeff_t> &rhs)
+  {
+    Polynomial<coeff_t> result = *this;
+    result *= rhs;
+    return result;
+  }
+
+  Polynomial<coeff_t> operator*(const coeff_t &rhs)
+  {
+    Polynomial<coeff_t> result = *this;
+    result *= rhs;
+    return result;
+  }
+
+  //Polynomial<coeff_t> operator+(const Polynomial<coeff_t> &other);
+  //Polynomial<coeff_t> operator*(const Polynomial<coeff_t> &other);
+  //Polynomial<coeff_t> operator-(const Polynomial<coeff_t> &other);
+  //Polynomial<coeff_t> operator+=(const Polynomial<coeff_t> &other);
+  //Polynomial<coeff_t> operator*=(const Polynomial<coeff_t> &other);
   //Polynomial<coeff_t> operator*=(const coeff_t &scalar);
-  Polynomial<coeff_t> operator-=(const Polynomial<coeff_t> &other);
+  //Polynomial<coeff_t> operator-=(const Polynomial<coeff_t> &other);
   //Polynomial<coeff_t> operator*(const coeff_t &scalar);
-  
+
 };
 
-template<class coeff_t>
+/*
+template<typename coeff_t>
 Polynomial<coeff_t>& operator=(const Polynomial<coeff_t> &rhs)
 {
   if(this == &rhs)
     return *this;
   coeff.resize(rhs.size());
-  set_coeffs(rhs); 
+  set_coeffs(rhs);
 }
+*/
 
-// Type-specific stuff: constructors and (some) arithmetic
-// Constructors need to be type-specific because there are different types of zeros, and this ain't Julia
-
+template<>
 Polynomial<double>::Polynomial()
 {
   coeff = std::vector<double>{0.};
 }
 
-Polynomial<long>::Polynomial()
+/*
+int main()
 {
-  coeff = std::vector<long>{0};
+  Polynomial<double> p;
+  Polynomial<double> q;
+  Polynomial<double> r;
+  std::vector<double> v{3., 4., 5.};
+  p.set_coeffs(v);
+  
+  std::cout << p.degree() << std::endl;
+  std::cout << p(7.5) << std::endl;
+  q = p;
+  r = p * q;
+  std::cout << r.degree() << std::endl;
+  std::cout << r(3.) << std::endl;
 }
-
-Polynomial<complex<double> >::Polynomial()
-{
-  coeff = std::vector<complex<double >>{0.};
-}
-
-template<class coeff_t>
-Polynomial<coeff_t> & Polynomial<coeff_t>::operator+=( const Polynomial<coeff_t> &rhs )
-{
-  if (rhs.degree() > degree())
-  {
-    coeff.resize(rhs.degree());
-  }
-  for (int i = 0; i <= rhs.degree(); i++)
-  {
-    coeff[i] += rhs.coeff[i];
-  }
-  return *this;
-}
-
-template<class coeff_t>
-Polynomial<coeff_t> & Polynomial<coeff_t>::operator-=( const Polynomial<coeff_t> &rhs )
-{
-if (rhs.degree() > degree())
-  { 
-    coeff.resize(rhs.degree());
-  } 
-  for (int i = 0; i <= rhs.degree(); i++)
-  {
-    coeff[i] -= rhs.coeff[i];
-  }  
-  return *this;
-}
-
-template<class coeff_t>
-Polynomial<coeff_t> & Polynomial<coeff_t>::operator*=( const Polynomial<coeff_t> &rhs )
-{
-  long new_degree = degree() + rhs.degree();
-  coeff.resize(new_degree);
-  std::vector<coeff_t> tempc(new_degree);
-  int i;
-  int j;
-  for (i = 0; i <= new_degree; i++)
-  {
-    for (j = 0; j <= i; j++)
-    {
-      if (j <= degree() && (i - j) <= rhs.degree())
-        tempc[i] += coeff[j] * rhs.coeff[i - j];
-    }
-  }
-  set_coeffs(tempc);
-  return *this;
-}
-
-template<class coeff_t>
-Polynomial<coeff_t> Polynomial<coeff_t>::operator+(const Polynomial<coeff_t> &rhs)
-{
-  Polynomial<coeff_t> result = *this;
-  result += rhs;
-  return result;
-}
-
-template<class coeff_t>
-Polynomial<coeff_t> Polynomial<coeff_t>::operator-
-{
-  Polynomial<coeff_t> result = *this;
-  result -= rhs;
-  return result;
-}
-
-template<class coeff_t>
-Polynomial<coeff_t> Polynomial<coeff_t>::operator*
-{
-  Polynomial<coeff_t> result = *this;
-  result *= rhs;
-  return result;
-}
-
+*/
